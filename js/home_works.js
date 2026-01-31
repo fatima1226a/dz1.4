a22a7e3c51b968b853e25a148313c6b5e3a1b86c
Fatima <famanturova@gmail.com>
2026-01-31 21:29:55 +0600

    dz2

--- js/home_works.js
+++ js/home_works.js
@@ -1,3 +1,5 @@
+// GMAIL VALIDATION
+
 const gmailInput = document.querySelector("#gmail_input")
 const gmailButton = document.querySelector("#gmail_button")
 const gmailResult = document.querySelector("#gmail_result")
@@ -16,17 +18,74 @@ gmailButton.onclick = () => {
 
 
 
+// MOVE BLOCK
+
+
+const childBlock = document.querySelector(".child_block")
+const parentBlock = document.querySelector(".parent_block")
+
+let positionX = 0
+let positionY = 0
+
+const offsetWidth = parentBlock.clientWidth - childBlock.clientWidth
+const offsetHeight = parentBlock.clientHeight - childBlock.clientHeight
 
 
 
-const block = document.querySelector(".child_block");
-let posision = 0;
 const move = () => {
-    posision++;
-    block.style.left = posision + "px";
-    if (posision < 448) {
-        requestAnimationFrame(move)
-    }
+    if (positionX < offsetWidth && positionY === 0) 
 
-};
-move();
+        { positionX++}
+
+    else if (positionX === offsetWidth && positionY < offsetHeight) 
+         {positionY++}
+
+    else if  (positionY === offsetHeight && positionX > 0)
+
+         {positionX--}
+        
+    else if  (positionX === 0 && positionY > 0)
+        {positionY--}
+
+
+    childBlock.style.left = `${positionX}px`
+    childBlock.style.top = `${positionY}px`
+    
+    requestAnimationFrame(move)
+}
+
+move()
+
+
+
+//TIMER
+
+const seconds = document.querySelector("#seconds")
+const startbtn = document.querySelector("#start")
+const stopbtn = document.querySelector("#stop")
+const resetbtn = document.querySelector("#reset")
+
+let sec = 0
+let interval = null
+
+startbtn.onclick = () => {
+    if (!interval){
+        interval = setInterval (() => {
+         sec++ 
+         seconds.innerText = sec;
+        }, 1000)
+   
+    }
+}
+
+stopbtn.onclick = () => {
+    clearInterval(interval); 
+    interval = null
+}
+
+resetbtn.onclick = () => {
+    clearInterval(interval);
+    interval = null;
+    sec = 0
+    seconds.innerText = 0;
+}
